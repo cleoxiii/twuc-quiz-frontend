6f73d17c1ed009b54d6f4cc882d2c20f26a8f209
Yun Chenxi <cleoxiii@163.com>
2020-08-14 15:47:43 +0800

    [yunchenxi] feat: finish static header, and only contains mall.

---
+++ src/Header/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../Header/Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <ul>
+          <li>商城</li>
+        </ul>
+      </header>
+    );
+  }
+}
+
+export default Header;
