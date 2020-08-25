c0def76a0535344b3c5959a16ba420a99cb27721
Yun Chenxi <cleoxiii@163.com>
2020-08-25 20:16:41 +0800

    [yunchenxi] refactor: make display of button clear when it is disabled.

--- src/components/SingleProduct/SingleProduct.js
+++ src/components/SingleProduct/SingleProduct.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './SingleProduct.css';
+import { Button } from "antd";
 
 class SingleProduct extends Component {
   render() {
@@ -7,10 +8,14 @@ class SingleProduct extends Component {
       <div className="single-product">
         <img src={this.props.imageUrl} alt={this.props.name}/>
         <h4>{this.props.name}</h4>
-        <p>单价：{this.props.price}元/{this.props.unit}</p>
-        <button
-          disabled={this.props.isDisabled}
-          onClick={() => this.props.onAddProduct(this.props.id)}>十</button>
+        <div className="bottom">
+          <p>单价：{this.props.price}元/{this.props.unit}</p>
+          <Button
+            type="primary"
+            shape="circle"
+            disabled={this.props.isDisabled}
+            onClick={() => this.props.onAddProduct(this.props.id)}>+</Button>
+        </div>
       </div>
     );
   }
